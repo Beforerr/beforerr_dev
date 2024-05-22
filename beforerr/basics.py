6612e116e86b5fba03c572e97a25b1f49283292a
Beforerr <zzj956959688@gmail.com>
2024-05-22 16:37:06 -0700

    chore: Update matplotlib module with new functions for unifying axis fontsize

--- beforerr/basics.py
+++ beforerr/basics.py
@@ -5,7 +5,7 @@ __all__ = ['pmap']
 
 # %% ../nbs/01_basics.ipynb 2
 from pipe import select
-from fastcore.utils import partial
+from functools import partial
 
 # %% ../nbs/01_basics.ipynb 3
 def pmap(func, *args, **kwargs):
